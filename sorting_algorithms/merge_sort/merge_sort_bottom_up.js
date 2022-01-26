34128447644a5044c5b4659c1e0f46200cce54cc
Ishkhanuhi <hakobian18@gmail.com>
2022-01-26 14:40:23 +0400

    done

--- sorting_algorithms/merge_sort/merge_sort_bottom_up.js
+++ sorting_algorithms/merge_sort/merge_sort_bottom_up.js
@@ -12,16 +12,11 @@ function merge(left, right) {
   }
 
   result = result.concat(left).concat(right);
-
-  //make sure remaining arrays are empty
-  left.splice(0, left.length);
-  right.splice(0, right.length);
   console.log("Result: " + result);
   return result;
 }
 
 function mergeSort(items) {
-  // Terminal condition - don't need to do anything for arrays with 0 or 1 items
   if (items.length < 2) {
     return items;
   }
@@ -29,14 +24,12 @@ function mergeSort(items) {
   for (let i = 0; i < items.length; i++) {
     work.push([items[i]]);
   }
-  work.push([]); //in case of odd number of items
 
   for (let lim = items.length; lim > 1; lim = Math.floor((lim + 1) / 2)) {
     for (var j = 0, k = 0; k < lim; j++, k += 2) {
       work[j] = merge(work[k], work[k + 1]);
       console.log(`Work[${j}] = ` + work[j]);
     }
-    work[j] = []; //in case of odd number of items
   }
 
   return work[0];
